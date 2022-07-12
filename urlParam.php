9ae67a36d425117364a9e8c72038a83cb912c7ef
박형주 <hyeongju1122@gmail.com>
2022-07-13 00:55:50 +0900

    PHP URL Parameter

---
+++ urlParam.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang=kr>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>URL Param</title>
+</head>
+<body>
+    <h1>URL Parameter</h1>
+    <h2>Hello '<?php echo $_GET['name']; ?>'! <br>
+    Welcome to my First PHP HomePage!</h2>
+    <h4>$_GET['파라미터']</h4>
+    <p>url에 name=page에 나왔으면 하는 이름 형식으로 작성합니다.</p>
+</body>
+</html>
