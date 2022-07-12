ae673558adb14be8b661d3dbcafc249e2b8d0ff6
박형주 <hyeongju1122@gmail.com>
2022-07-13 00:34:30 +0900

    PHP Variable

--- valiable.php
+++ valiable.php
@@ -10,6 +10,12 @@
 <body>
     <h1>Variable - 변수</h1>
     <h2>php에서는 변수선언을 <strong>$</strong>를 쓴다!</h2>
-
+    <?php
+    $name = "hyeongjuPARK";
+    echo "Lorem ipsum dolor sit amet, ".$name." adipisicing elit.</br> 
+    Adipisci aspernatur blanditiis commodi".$name." cupiditate delectus deleniti ducimus et facere,<br>
+     harum ipsum iste laudantium maxime odit ".$name." pariatur quaerat reiciendis ut veniam vero?<br>";
+    ?>
+    <h4><strong>"문자열".$변수명."문자열" 형식으로 사용됨!</strong></h4>
 </body>
 </html>
