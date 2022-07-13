0f114e218bd4a96b0f93a4250f479b27b4962163
박형주 <hyeongju1122@gmail.com>
2022-07-13 23:27:37 +0900

    conditional + run time error sorved

--- comparison.php
+++ comparison.php
@@ -9,8 +9,18 @@
 </head>
 <body>
     <h1>Comparison Operators &amp; Boolean data type</h1>
+    <h2>2 < 1</h2>
     <?php
         var_dump(2 < 1);
     ?>
+    <h2>1 == 1</h2>
+    <?php
+        var_dump(1 == 1);
+    ?>
+    <h3>다음과 같이 var_dump함수를 이용하면 참과 거짓을 리턴한다.</h3>
+    <?php
+        var_dump("hyeongjuPARK");
+    ?>
+    <h2>var_dump는 자료형과 갯수또한 리턴해준다.</h2>
 </body>
 </html>
---
+++ conditional.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="kr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>conditional</title>
+</head>
+<body>
+    <h1>Conditional</h1>
+    <h2>if</h2>
+    <?php
+        echo '1</br>';
+        if (4 < 1) {
+            echo '2<br/>';
+        } else {
+            print 'else<br/>';
+        }
+        echo '3<br/>';
+    ?>
+</body>
+</html>
